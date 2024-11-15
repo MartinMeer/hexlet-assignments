49eb9a6bebce20b972bfb9e43b346fa50d354cdf
@hexlet/cli <support@hexlet.io>
2024-11-15 13:27:03 +0300

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-/ BEGIN
+// BEGIN
 public class App {
     /**Метод сортирует объекты по площади по возрастанию,
      * берет первые n элементов и возвращает строковые представления этих объектов в виде списка List.
